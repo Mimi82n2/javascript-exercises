71f560a3805b13331d2082e7d45df0eccc4d6379
Jimmy Chung <cheming@ualberta.ca>
2023-09-03 19:45:37 -0700

    Complete JavaScript Exercises 1-7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
+    // res = "";
+    // for (let i = string.length-1; i>=0; i--){
+    //     res += string[i];
+    // }
+    // return res
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(s, e) {
+    if (typeof(s) !== 'number' || typeof(e) !== 'number' || s < 0 || e < 0){
+        return "ERROR";
+    }
+    total = 0;
+    if (e < s){
+        [s,e] = [e,s];
+    }
+    for (let i = s; i <= e; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
