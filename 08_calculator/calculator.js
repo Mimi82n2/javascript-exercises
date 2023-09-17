b4022b861c12f4d62a8e2bce023069230be11672
Jimmy Chung <cheming@ualberta.ca>
2023-09-16 20:19:41 -0700

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let res = arr.reduce((total, n) => {
+    return total + n;
+  }, 0);
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let res = nums.reduce((prod, n) => {
+    return prod * n;
+  });
+  return res
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  res = 1;
+  while (a > 0){
+    res *= a;
+    a -= 1;
+  }
+  return res;
+
 };
 
 // Do not edit below this line
