b082e8d5dfb231e95015773bbee342158d6df30e
Jimmy Chung <cheming@ualberta.ca>
2023-09-16 20:44:03 -0700

    10 Fibo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n <= 0){
+        return "OOPS";
+    }
+    let a = 1;
+    let b = 1;
 
+    for (let i = 0; i < n-1; i++){
+        let temp = a;
+        a = b
+        b = temp + b;
+    }
+    return a;
 };
 
 // Do not edit below this line
