cb081e72b2c69095d71053b9cb6687f5f67e92c3
Jimmy Chung <cheming@ualberta.ca>
2023-09-17 17:01:37 -0700

    11 + 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    a = people.reduce((oldest, person) => {
+        let oldDeath;
+        if(!(oldest.yearOfDeath)){
+            oldDeath = new Date().getFullYear();
+        }else{
+            oldDeath = oldest.yearOfDeath
+        }
 
+        let perDeath;
+        if(!(person.yearOfDeath)){
+            perDeath = new Date().getFullYear();
+        }else{
+            perDeath = person.yearOfDeath
+        }
+
+        if(perDeath - person.yearOfBirth > oldDeath - oldest.yearOfBirth){
+            oldest = person;
+        };
+        return oldest;
+    });
+    return a;
 };
 
 // Do not edit below this line
